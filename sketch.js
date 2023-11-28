4ec0e4370b13211cc91497f2a6d3ed72b02f5487
AlexaZ06 <alexwbaby@hotmail.com>
2023-11-28 12:15:18 -0600

    create general grid class for making other images

--- sketch.js
+++ sketch.js
@@ -173,10 +173,9 @@ class Gridcat {
       }
     }
   }
-
 }
 
-class Gridwolf extends Gridcat {
+class Grid extends Gridcat {
   constructor (img) {
     super(img);
   }
@@ -192,7 +191,207 @@ class Gridwolf extends Gridcat {
   }
 
   //assign numbers to colours
-  numberImage
+  numberImage(cols, rows) {
+    let blocknum = [];
+    for (let y = 0; y < rows; y++) {
+      blocknum.push([]);
+      for (let x = 0; x < cols; x++) {
+  
+        //white
+        if (this.imgColour[y][x][0] >= 210 && this.imgColour[y][x][1] >= 210 && this.imgColour[y][x][2] >= 210
+          || this.imgColour[y][x][3] <= 40) {
+          blocknum[y].push(0);
+        }
+  
+        //light grey
+        else if (this.imgColour[y][x][0] < 210 && this.imgColour[y][x][0] >= 140 &&
+          this.imgColour[y][x][1] < 210 && this.imgColour[y][x][1] >= 140 &&
+          this.imgColour[y][x][2] < 210 && this.imgColour[y][x][2] >= 140) {
+          blocknum[y].push(1);
+        }
+  
+        //dark grey
+        else if (this.imgColour[y][x][0] < 140 && this.imgColour[y][x][0] > 40 &&
+          this.imgColour[y][x][1] < 140 && this.imgColour[y][x][1] > 40 &&
+          this.imgColour[y][x][2] < 140 && this.imgColour[y][x][2] > 40) {
+          blocknum[y].push(2);
+        }
+  
+        //red 
+        else if (this.imgColour[y][x][0] > this.imgColour[y][x][1] && this.imgColour[y][x][0] > this.imgColour[y][x][2]) {
+          blocknum[y].push(3);
+        }
+
+        //yellow 
+        else if (this.imgColour[y][x][0] >= 250 && this.imgColour[y][x][1] >= 200
+          && this.imgColour[y][x][0] > this.imgColour[y][x][2]
+          && this.imgColour[y][x][1] > this.imgColour[y][x][2]) {
+          blocknum[y].push(4);
+        }
+
+        //orange 
+        else if (this.imgColour[y][x][0] >= 225 && this.imgColour[y][x][1] >= 175
+                 && this.imgColour[y][x][0] > this.imgColour[y][x][2]
+                 && this.imgColour[y][x][1] > this.imgColour[y][x][2]) {
+          blocknum[y].push(5);
+        }
+
+        //green 
+        else if (this.imgColour[y][x][1] > this.imgColour[y][x][0] && this.imgColour[y][x][1] > this.imgColour[y][x][2]) {
+          blocknum[y].push(6);
+        }
+
+        //blue 
+        else if (this.imgColour[y][x][2] > this.imgColour[y][x][0] && this.imgColour[y][x][2] > this.imgColour[y][x][1]) {
+          blocknum[y].push(7);
+        }
+
+        //purple 
+        else if (this.imgColour[y][x][0] > this.imgColour[y][x][1] && this.imgColour[y][x][2] > this.imgColour[y][x][1]) {
+          blocknum[y].push(8);
+        }
+  
+        //black
+        else if (this.imgColour[y][x][0] <= 40 && this.imgColour[y][x][1] <= 40 && this.imgColour[y][x][2] <= 40
+          || this.imgColour[y][x][3] >= 200) {
+          blocknum[y].push(9);
+        }
+  
+      }
+    }
+    return blocknum;
+  }
+
+  //change cell when clicked
+  toggleCell(x,y) {
+    //check that we are within the grid, then toggle
+    if (x >= 0 && x < this.GRID_SIZE && y >= 0 && y < this.GRID_SIZE) {
+      // check for right colour then toggle to colour
+      if (this.blockNumber[y][x] < 10) {
+        if (this.colour === 0 && this.blockNumber[y][x] === 0) {
+          this.blockNumber[y][x] += 10;
+        }
+        else if (this.colour === 1 && this.blockNumber[y][x] === 1) {
+          this.blockNumber[y][x] += 11;
+        }
+        else if (this.colour === 2 && this.blockNumber[y][x] === 2) {
+          this.blockNumber[y][x] += 20;
+        }
+        else if (this.colour === 3 && this.blockNumber[y][x] === 3) {
+          this.blockNumber[y][x] += 30;
+        }
+        else if (this.colour === 4 && this.blockNumber[y][x] === 4) {
+          this.blockNumber[y][x] += 40;
+        }
+        else if (this.colour === 5 && this.blockNumber[y][x] === 5) {
+          this.blockNumber[y][x] += 50;
+        }
+        else if (this.colour === 6 && this.blockNumber[y][x] === 6) {
+          this.blockNumber[y][x] += 60;
+        }
+        else if (this.colour === 7 && this.blockNumber[y][x] === 7) {
+          this.blockNumber[y][x] += 70;
+        }
+        else if (this.colour === 8 && this.blockNumber[y][x] === 8) {
+          this.blockNumber[y][x] += 80;
+        }
+        else if (this.colour === 9 && this.blockNumber[y][x] === 9) {
+          this.blockNumber[y][x] += 90;
+        }
+      }
+      //change block back to original colour
+      else if (this.blockNumber[y][x] >= 10) {
+        if (this.colour === 0 && this.blockNumber[y][x] === 10) {
+          this.blockNumber[y][x] -= 10;
+        }
+        else if (this.colour === 1 && this.blockNumber[y][x] === 12) {
+          this.blockNumber[y][x] -= 11;
+        }
+        else if (this.colour === 2 && this.blockNumber[y][x] === 22) {
+          this.blockNumber[y][x] -= 20;
+        }
+        else if (this.colour === 3 && this.blockNumber[y][x] === 33) {
+          this.blockNumber[y][x] -= 30;
+        }
+        else if (this.colour === 4 && this.blockNumber[y][x] === 44) {
+          this.blockNumber[y][x] -= 40;
+        }
+        else if (this.colour === 5 && this.blockNumber[y][x] === 55) {
+          this.blockNumber[y][x] -= 50;
+        }
+        else if (this.colour === 6 && this.blockNumber[y][x] === 66) {
+          this.blockNumber[y][x] -= 60;
+        }
+        else if (this.colour === 7 && this.blockNumber[y][x] === 77) {
+          this.blockNumber[y][x] -= 70;
+        }
+        else if (this.colour === 8 && this.blockNumber[y][x] === 88) {
+          this.blockNumber[y][x] -= 80;
+        }
+        else if (this.colour === 9 && this.blockNumber[y][x] === 99) {
+          this.blockNumber[y][x] -= 90;
+        }
+      }
+    }
+  }
+
+  //show grid
+  displayGrid() {
+    for (let y = 0; y < this.GRID_SIZE; y++) {
+      for (let x = 0; x < this.GRID_SIZE; x++) {
+        if (this.GRID_SIZE >= 40) {
+          //block colour
+          fill("lightblue");
+          rect(x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize);
+  
+          if (this.blockNumber[y][x] < 10) {
+            //fill in grid with numbers
+            fill("black");
+            textAlign(CENTER, CENTER);
+            text(this.blockNumber[y][x], x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize);
+          }
+          else {
+            //colour/fill in cells
+            if (this.blockNumber[y][x] === 10) {
+              fill("white");
+            }
+            else if (this.blockNumber[y][x] === 12) {
+              fill("lightgrey");
+            }
+            else if (this.blockNumber[y][x] === 22) {
+              fill("grey");
+            }
+            else if (this.blockNumber[y][x] === 33) {
+              fill("pink");
+            }
+            else if (this.blockNumber[y][x] === 44) {
+              fill("lightyellow");
+            }
+            else if (this.blockNumber[y][x] === 55) {
+              fill("lightorange");
+            }
+            else if (this.blockNumber[y][x] === 66) {
+              fill("lightgreed");
+            }
+            else if (this.blockNumber[y][x] === 77) {
+              fill("lightblue");
+            }
+            else if (this.blockNumber[y][x] === 88) {
+              fill("lavender");
+            }
+            else if (this.blockNumber[y][x] === 99) {
+              fill("black");
+            }
+            rect(x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize);
+          }
+        }
+  
+        else {
+          rect(x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize);
+        }
+      }
+    }
+  }
 }
 
 //load image to colour
